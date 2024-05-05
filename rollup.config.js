45758b091956946677c63ea73d690a9691e8d457
Tobias Rademacher <npm.install.rademacher@gmail.com>
2024-05-05 14:20:57 +0200

    jest and storybook added

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
+// for css imports
+import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
@@ -21,14 +26,22 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      // for css imports
+      postcss(),
+      terser(),
     ],
+    external: ["react-dom"],
   },
+  // this part is for creating the d.ts files only
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    // to ignore css files, as they can be ignored for type creation
+    external: [/\.css$/],
   },
 ];
